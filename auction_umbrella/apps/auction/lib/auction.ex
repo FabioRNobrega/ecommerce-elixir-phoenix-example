ce97c01743c6193b27cb0fde9eb0e8f94c521ac3
Fábio R. Nóbrega <fabio.r.nobrega@gmail.com>
2022-10-27 08:25:30 -0300

    feat(setup): create umbrella application and first app

---
+++ auction_umbrella/apps/auction/lib/auction.ex
@@ -0,0 +1,18 @@
+defmodule Auction do
+  @moduledoc """
+  Documentation for `Auction`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Auction.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
