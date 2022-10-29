27e20706ed61969e76b3ccd8dfe921d02c348ff5
Fábio R. Nóbrega <fabio.r.nobrega@gmail.com>
2022-10-29 16:19:04 -0300

    feat(auction_umbrella/apps/auction/): setup ecto and simple CRD)

--- auction_umbrella/config/config.exs
+++ auction_umbrella/config/config.exs
@@ -44,6 +44,16 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Config Ecto database
+config :auction, ecto_repos: [Auction.Repo]
+
+config :auction, Auction.Repo,
+  database: "auction",
+  username: "user",
+  password: "password",
+  hostname: "localhost",
+  port: "5432"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
